79668e4a88565cbe866fdd5685848402b760a170
David <davidgcastro93@gmail.com>
2018-07-07 18:08:09 -0400

    finished category

--- components/Slide.js
+++ components/Slide.js
@@ -1,19 +1,49 @@
-import React, { Component } from 'react';
-import { View, Image, Text } from 'react-native';
+import React from 'react';
+import { View, Image } from 'react-native';
 import styles from '../styles/slider';
+import { Badge, Text } from 'native-base';
+
 import Swiper from 'react-native-swiper';
 
 const Slider = props => (
-  <View
-    style={{
-      alignContent: 'center',
-      alignItems: 'center'
-    }}>
-    <Image
-      style={styles.image}
-      source={require('../assets/images/newyork-apt.jpg')}
-    />
-    <Text> NightLife </Text>
+  <View>
+    <View style={{ position: 'relative' }}>
+      <Image
+        style={styles.image}
+        source={require('../assets/images/newyork-apt.jpg')}
+      />
+    </View>
+    <Text
+      style={{
+        position: 'absolute',
+        color: 'white',
+        borderWidth: 0,
+        fontSize: 15,
+        top: 20,
+        left: 20,
+        borderRadius: 5,
+        paddingLeft: 10,
+        paddingRight: 10,
+        paddingBottom: 5,
+        paddingTop: 5,
+        overflow: 'hidden',
+        backgroundColor: '#FF5A5F',
+        letterSpacing: 0.5
+      }}>
+      NIGHTLIFE
+    </Text>
+    <Badge
+      success
+      style={{
+        position: 'absolute',
+        top: 40,
+        left: 120,
+        backgroundColor: '#FF5A5F',
+        borderColor: 'white',
+        borderWidth: 1
+      }}>
+      <Text>2</Text>
+    </Badge>
   </View>
 );
 
--- styles/slider.js
+++ styles/slider.js
@@ -1,14 +1,13 @@
 import { Dimensions } from 'react-native';
-const { height } = Dimensions.get('window');
+const { width, height } = Dimensions.get('window');
 
 const styles = {
+  
   image: {
-    width: 320,
+    width: width - 40,
     height: height - 200,
     borderRadius: 20,
-    shadowOffset: { width: 10, height: 10 },
-    shadowColor: 'black',
-    shadowOpacity: 1.0
+ 
   }
 };
 
